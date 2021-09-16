29ef68a513ac53a291fe628afd4b01087591f301
kipit <k_github@42l.fr>
2021-09-16 19:14:26 +0200

    change config to fix bug with metamask

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,5 +6,14 @@ require("@nomiclabs/hardhat-waffle");
 require("./tasks/faucet");
 
 module.exports = {
-  solidity: "0.7.3"
+  solidity: "0.7.3",
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+    localhost: {
+      url: "http://localhost:8545",
+      chainId: 1337
+    },
+  }
 };
