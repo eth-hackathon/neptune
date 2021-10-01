992f93f5f3658b35e1f2b5121d6d5a277803dff4
IsraelMuca <isra.munoz@gmail.com>
2021-10-01 09:54:03 -0700

    feat: Overview page cleanup

--- frontend/src/pages/Dapp/Overview.js
+++ frontend/src/pages/Dapp/Overview.js
@@ -4,69 +4,33 @@ import discord from "image/discord.svg";
 import twitter from "image/twitter.svg"; */
 import {NavLink} from "react-router-dom";
 
+const ProtocolCard = ({to, logo, alt, name}) => {
+  const url = `/dapp/overview/uniswap/${to}`;
+
+  return (
+    <NavLink to={url}>
+      <div className="rounded-lg bg-white p-5">
+        <div className="flex flex-col items-center">
+          <img className="bg-gray-100 rounded-full w-20 h-20 m-3" src={logo} alt={alt} />
+          <p className="text-3xl font-bold">{name}</p>
+        </div>
+      </div>
+    </NavLink>
+  );
+};
+
 const Overview = () => {
   return (
     <main className="px-14 flex-col grid grid-cols-4 gap-6">
-      <NavLink to="/dapp/overview/uniswap">
-        <div className="rounded-lg mt-5 p-5 bg-white">
-          <div className="flex flex-col items-center">
-            <img
-              className="bg-gray-100 rounded-full w-16 h-16"
-              src={logo}
-              alt="uniswap-logo"
-            />
-            <p className="text-2xl font-semibold">Uniswap</p>
-          </div>
-        </div>
-      </NavLink>
-      <NavLink to="/dapp/overview/uniswap">
-        <div className="rounded-lg mt-5 p-5 bg-white">
-          <div className="flex flex-col items-center">
-            <img
-              className="bg-gray-100 rounded-full w-16 h-16"
-              src={logo}
-              alt="uniswap-logo"
-            />
-            <p className="text-2xl font-semibold">Uniswap</p>
-          </div>
-        </div>
-      </NavLink>
-      <NavLink to="/dapp/overview/uniswap">
-        <div className="rounded-lg mt-5 p-5 bg-white">
-          <div className="flex flex-col items-center">
-            <img
-              className="bg-gray-100 rounded-full w-16 h-16"
-              src={logo}
-              alt="uniswap-logo"
-            />
-            <p className="text-2xl font-semibold">Uniswap</p>
-          </div>
-        </div>
-      </NavLink>
-      <NavLink to="/dapp/overview/uniswap">
-        <div className="rounded-lg mt-5 p-5 bg-white">
-          <div className="flex flex-col items-center">
-            <img
-              className="bg-gray-100 rounded-full w-16 h-16"
-              src={logo}
-              alt="uniswap-logo"
-            />
-            <p className="text-2xl font-semibold">Uniswap</p>
-          </div>
-        </div>
-      </NavLink>
-      <NavLink to="/dapp/overview/uniswap">
-        <div className="rounded-lg mt-5 p-5 bg-white">
-          <div className="flex flex-col items-center">
-            <img
-              className="bg-gray-100 rounded-full w-16 h-16"
-              src={logo}
-              alt="uniswap-logo"
-            />
-            <p className="text-2xl font-semibold">Uniswap</p>
-          </div>
-        </div>
-      </NavLink>
+      {[...Array(5)].map((_, i) => (
+        <ProtocolCard
+          to="uniswap"
+          logo={logo}
+          alt="Uniswap Logo"
+          name="Uniswap"
+          key={i}
+        />
+      ))}
     </main>
   );
 };
