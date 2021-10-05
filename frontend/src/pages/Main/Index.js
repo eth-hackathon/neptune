9c697eeac470bf8f137d274b0797d09ed33ca09a
IsraelMuca <isra.munoz@gmail.com>
2021-10-05 12:52:34 -0700

    fix: updated CSS for main page

--- frontend/src/pages/Main/Index.js
+++ frontend/src/pages/Main/Index.js
@@ -2,11 +2,12 @@ import SearchBar from "components/SearchBar.js";
 
 const Index = () => (
   <main>
-    <div className="lg:text-center">
-      <p className="text-7xl px-50 text-indigo-600 tracking-wide font-extrabold mt-40">
+    <div className="text-center max-w-2xl mx-auto">
+      <p className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl px-50 text-indigo-600 tracking-wide font-extrabold mt-40">
         Incentive layer on StackOverflow
       </p>
     </div>
+
     <div class="mt-5">
       <SearchBar />
     </div>
