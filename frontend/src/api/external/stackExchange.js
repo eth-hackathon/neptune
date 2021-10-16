be75518cbfcd9724a0ce502ddc0ef8f469d4aeef
IsraelMuca <isra.munoz@gmail.com>
2021-10-16 13:48:38 -0500

    fix: api calls need new axios instance, otherwise `baseUrl` clashes and causes incorrect calls

--- frontend/src/api/external/stackExchange.js
+++ frontend/src/api/external/stackExchange.js
@@ -1,15 +1,18 @@
 import {add, isFuture} from "date-fns";
 import axios from "axios";
 
-axios.defaults.baseURL = "https://api.stackexchange.com/2.3/";
+const stackAxios = axios.create({
+  baseURL: "https://api.stackexchange.com/2.3/",
+});
 
-async function getUserInfo(idx) {
+async function getUserInfo() {
   const access_token = localStorage.getItem("access_token");
   const expires = localStorage.getItem("expires");
 
   if (!access_token || !expires) {
     console.log("no token error");
     return {
+      data: null,
       error: "The user has not authenticated.",
     };
   }
@@ -17,24 +20,27 @@ async function getUserInfo(idx) {
   const today = new Date();
   const expires_in = add(today, {seconds: expires});
   if (!isFuture(expires_in)) {
-    console.log("error");
+    console.log("date error");
 
-    return {error: "The token has expired."};
+    return {data: null, error: "The token has expired."};
   }
 
   // add key, site and access_token
   try {
-      const response = await axios({
-        method: "get",
-        url: "/me/",
-        params: { access_token },
-      });
+    const response = await stackAxios({
+      method: "get",
+      url: "/me/",
+      params: {access_token},
+    });
 
-      return response.data;
-    } catch (error) {
-      console.error(error);
-    }
+    return {
+      data: response.data,
+      error: null,
+    };
+  } catch (error) {
+    return {data: null, error: error};
   }
+}
 
 // ?key=U4DMV*8nvpm3EOpvf69Rxw((
 
--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,20 +1,22 @@
 import axios from "axios";
-// This will be put in a .env
-axios.defaults.baseURL = "http://localhost:8080";
+
+const mainAxios = axios.create({
+  baseURL: "http://localhost:8080",
+});
 
 // Example of a Get request. When you send a params object you will receive a
 // query object in Express
 async function getUser(params) {
   try {
-    const response = await axios({
+    const response = await mainAxios({
       method: "get",
       url: "/api/user",
       params: params,
     });
 
-    return response.data;
+    return {data: response.data, error: null};
   } catch (error) {
-    console.error(error);
+    return {data: null, error: error};
   }
 }
 
@@ -22,41 +24,41 @@ async function getUser(params) {
 // body object in Express
 async function addUser(body) {
   try {
-    const response = await axios({
+    const response = await mainAxios({
       method: "post",
       url: "/api/user",
       data: body,
     });
 
-    return response.data;
+    return {data: response.data, error: null};
   } catch (error) {
-    console.error(error);
+    return {data: null, error: error};
   }
 }
 
 async function getServerDID() {
   try {
-    const response = await axios({
+    const response = await mainAxios({
       method: "get",
       url: "/api/server-did",
     });
 
-    return response.data;
+    return {data: response.data, error: null};
   } catch (error) {
-    console.log(error);
+    return {data: null, error: error};
   }
 }
 
 async function getJsonModel() {
   try {
-    const response = await axios({
+    const response = await mainAxios({
       method: "get",
       url: "/api/json-model",
     });
 
-    return response.data;
+    return {data: response.data, error: null};
   } catch (error) {
-    console.log(error);
+    return {data: null, error: error};
   }
 }
 
